363ad5906db004955ddd4302f0ef0dda88bb5a1c
MarcoCocco <marcococco28@gmail.com>
2023-05-09 16:27:02 +0200

    feat: creato il footer e aggiunto nel layout

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,61 @@ Route::get('/', function () {
         'shop'
     ];
 
-    return view('home', compact('comics', 'navLinks'));
+    $footerColumns = array(
+        array(
+            'title' => 'DC COMICS',
+            'links' => array(
+                'Characters',
+                'Comics',
+                'Movies',
+                'TV',
+                'Games',
+                'Videos',
+                'News'
+            )
+        ),
+        array(
+            'title' => 'SHOP',
+            'links' => array(
+                'Shop DC',
+                'Shop DC Collectibles'
+            )
+        ),
+        array(
+            'title' => 'DC',
+            'links' => array(
+                'Terms of Use',
+                'Provacy Policy (New)',
+                'Ad Choices',
+                'Advertising',
+                'Jobs',
+                'Subscriptions',
+                'Talent Workshops',
+                'CPSC Certificates',
+                'Ratings',
+                'Shop Help',
+                'Contact Us'
+            )
+        ),
+        array(
+            'title' => 'SITES',
+            'links' => array(
+                'DC',
+                'MAD Magazine',
+                'DC Kids',
+                'DC Universe',
+                'DC Power Visa'
+            )
+        )
+    );
+
+    $socials = [
+        'resources/img/footer-facebook.png',
+        'resources/img/footer-twitter.png',
+        'resources/img/footer-youtube.png',
+        'resources/img/footer-pinterest.png',
+        'resources/img/footer-periscope.png',
+    ];
+
+    return view('home', compact('comics', 'navLinks', 'footerColumns', 'socials'));
 });
