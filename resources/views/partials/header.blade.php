7f967e6586f8f5e0f3782f5afcdb5e3f9eb96833
MarcoCocco <marcococco28@gmail.com>
2023-05-09 14:33:54 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+        <nav>
+            <div class="navbar-container">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+                </div>
+                <div class="links">
+                    <ul>
+                        <li><strong>link</strong></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
